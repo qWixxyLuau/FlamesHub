0b6977774728c9fba9d201b023d5b01ca0c78e1a
qWixxyLuau <kiryuha4112@mail.ru>
2025-08-04 16:54:33 +0500

    Update SAB_Joiner.lua

--- SAB_Joiner.lua
+++ SAB_Joiner.lua
@@ -298,7 +298,7 @@ while wait(1) do
 	button.BackgroundColor3 = tab[tostring(_G.JoinerEnabled)]
 
 	if _G.JoinerEnabled == true then
-		queue_on_teleport("_G.JoinerEnabled = "..(_G.JoinerEnabled or false).." _G.Webhook = ".."'"..tostring(_G.Webhook).."'".." _G.Rarity = ".."'"..(_G.Rarity or "Secret").."'".." loadstring(game:HttpGet('https://raw.githubusercontent.com/qWixxyLuau/FlamesHub/refs/heads/main/SAB_Joiner.lua'))()")
+		queue_on_teleport(("_G.JoinerEnabled = "..(tostring(_G.JoinerEnabled) or false).." _G.Webhook = ".."'"..tostring(_G.Webhook).."'".." _G.Rarity = ".."'"..(_G.Rarity or "Secret").."'".." loadstring(game:HttpGet('https://raw.githubusercontent.com/qWixxyLuau/FlamesHub/refs/heads/main/SAB_Joiner.lua'))()"))
 		print("hop loop")
 		hop()
 	end
